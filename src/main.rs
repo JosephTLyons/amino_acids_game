1d0fb4ce3eac7c6e7fd980f37097d68785395d15
Joseph Lyons <josephtlyons@gmail.com>
2019-01-28 15:53:50 -0500

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -38,50 +38,50 @@ fn play_game() {
         "histidine"
     ];
 
-    println!("Type name of amino acid, \"hint\", or \"skip\"");
+    println! ("Type name of amino acid, \"hint\", or \"skip\"");
 
-    guess_amino_acids(&mut non_polar, "non-polar");
-    guess_amino_acids(&mut polar, "polar");
-    guess_amino_acids(&mut acidic, "acidid");
-    guess_amino_acids(&mut basic, "basic");
+    guess_amino_acids (&mut non_polar, "non-polar");
+    guess_amino_acids (&mut polar, "polar");
+    guess_amino_acids (&mut acidic, "acidid");
+    guess_amino_acids (&mut basic, "basic");
 
-    println!("The game is over!");
+    println! ("The game is over!");
 }
 
-fn guess_amino_acids(amino_acids: &mut Vec<&str>, amino_type: &str) {
+fn guess_amino_acids (amino_acids: &mut Vec<&str>, amino_type: &str) {
     let original_amount = amino_acids.len();
-    println!("Guess the {} {} amino acids:", original_amount, amino_type);
+    println! ("Guess the {} {} amino acids:", original_amount, amino_type);
 
     let mut input: String;
 
     while ! amino_acids.is_empty() {
-        println!("{}/{}", original_amount - amino_acids.len(), original_amount);
+        println! ("{}/{}", original_amount - amino_acids.len(), original_amount);
 
         input = read!();
 
         // Skip this group of amino acids
         if input == "skip" {
-            println!("The remaining {} amino acids are:", amino_type);
-            print_acids(&amino_acids);
+            println! ("The remaining {} amino acids are:", amino_type);
+            print_acids (&amino_acids);
             return;
         }
         // Print first first letter of first element as a hint
         else if input == "hint" {
-            println!("{}", amino_acids[0].as_bytes()[0] as char);
+            println! ("{}", amino_acids[0].as_bytes()[0] as char);
         }
 
         // Check if guess is correct
         for x in 0..amino_acids.len() {
             if amino_acids[x] == input.to_lowercase() {
-                amino_acids.remove(x);
+                amino_acids.remove (x);
                 break;
             }
         }
     }
 }
 
-fn print_acids(vec: &[&str]) {
+fn print_acids (vec: &[&str]) {
     for (x, item) in vec.iter().enumerate() {
-        println!("{}) {}", x + 1, item);
+        println! ("{}) {}", x + 1, item);
     }
 }
